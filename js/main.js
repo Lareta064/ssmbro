d85b12b20ae1dbf25b86aecbe7f3dcdd2aeedb53
Lareta <lareta1@yandex.ru>
2018-03-18 12:01:24 +0300

    commit-5

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    //плавная прокрутка
+    $("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
+      highlightSelector:"nav a"
+  	});
+})

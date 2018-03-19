825104e7f9702ffbb70032a771dc39177675f0b6
Lareta <lareta1@yandex.ru>
2018-03-19 19:53:51 +0300

    commit-6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+	//top-menu
+
+	var touch = $('.menu');
+		    var menu = $('.nav');
+		 
+		    $(touch).on('click', function(e) {
+		        e.preventDefault();
+		        menu.slideToggle();
+		    });
+		    $(window).resize(function(){
+		        var wid = $(window).width();
+		        if(wid > 760 && menu.is(':hidden')) {
+		            menu.removeAttr('style');
+		        }
+		    });
+	//top-menu
+
     //плавная прокрутка
     $("a,nav a,a[href='#'],a[rel='m_PageScroll2id'],a.PageScroll2id, a.mouse_scroll").mPageScroll2id({
       highlightSelector:"nav a"
